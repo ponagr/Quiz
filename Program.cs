2bd197d473a1dd3cccfeea99ca446da71db7e0a3
Pontus Ågren Grundström <pontus.agrengrundstrom@gmail.com>
2024-10-15 14:27:23 +0200

    Början på Menyval där metoder kommer att läggas till

--- Program.cs
+++ Program.cs
@@ -2,6 +2,31 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        bool runMenu = true;
+
+        while (runMenu)
+        {
+            Console.WriteLine("1. Lägg till en fråga");
+            Console.WriteLine("2. Kör Quiz");
+            Console.WriteLine("0. Avsluta Program");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    //Metod för att lägga till fråga
+                    break;
+                case 2:
+                    //Metod för att köra quiz
+                    break;
+                case 0:
+                    //Avsluta programmet
+                    runMenu = false;
+                    break;
+                default:
+                    Console.WriteLine("Något blev fel");
+                    break;
+            }
+        }
     }
 }
